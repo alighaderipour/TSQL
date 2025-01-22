5792b2a84f457445a3522f9ce9322a89e7ba87ab
Shahaz <ali.ghaderipour@gmail.com>
2025-01-22 05:40:07 +0330

    home
    1403-11-03 5:37

--- Northwind/test.sql
+++ Northwind/test.sql
@@ -1,10 +1,17 @@
 /*
 
-Show a list of all the different values in the Customers table for
-ContactTitles. Also include a count for each ContactTitle.
-This is similar in concept to the previous question “Countries where
-there are customers”
-, except we now want a count for each ContactTitle.
-
+For this problem, we’d like to see the total number of products in each
+category. Sort the results by the total number of products, in descending
+order.
 */
-
+SELECT
+  c.categoryname,
+  count(p.productid) as nb_products
+FROM
+  categories c
+INNER JOIN
+  products p on c.categoryid = p.categoryid
+GROUP BY
+  c.categoryname
+ORDER BY
+  nb_products DESC
