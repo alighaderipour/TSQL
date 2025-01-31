e61b2d7dcaa61a4e1e32b851e21b4a7bebec2d53
Shahaz <ali.ghaderipour@gmail.com>
2025-01-31 21:06:10 +0330

    home
    1403-11-12 21:06

--- _22_3_raise_error.sql
+++ _22_3_raise_error.sql
@@ -2,7 +2,7 @@ if OBJECT_ID('average_employee_amount', 'p') is not null
     drop PROC average_employee_amount
 go
 create proc average_employee_amount
-(@EmployeeFrom int, @EmployeeTo int , @AVG_Amount money OUTPUT, @TotalEmployee int OUTPUT)
+(@EmployeeFrom int, @EmployeeTo int , @AVG_Amount decimal(1,1) OUTPUT, @TotalEmployee int OUTPUT)
 AS
 BEGIN
     DECLARE @Total_Amount money
@@ -19,10 +19,17 @@ BEGIN
                     END
             END TRY
             BEGIN CATCH
-                set @AVG_Amount = -1
-                set @TotalEmployee = -1
-                SELECT ERROR_MESSAGE() as ERROR , ERROR_LINE() ERROR_LINE, ERROR_NUMBER() ERROR_NUMBER,
-                ERROR_PROCEDURE() ERROR_PROCEDURE, ERROR_SEVERITY() ERROR_SEVERITY,ERROR_STATE() ERROR_STATE
+                SELECT ERROR_NUMBER()
+                if ERROR_NUMBER() = 8134
+                    BEGIN
+                        SET @AVG_Amount = 0
+                        RETURN 8134
+                    END
+                ELSE
+                    RAISERROR ('Too Many Flanges' , 10, 1)
+                    --THROW 56789, 'Too many flanges', 1
+                --SELECT ERROR_MESSAGE() as ERROR , ERROR_LINE() ERROR_LINE, ERROR_NUMBER() ERROR_NUMBER,
+                --ERROR_PROCEDURE() ERROR_PROCEDURE, ERROR_SEVERITY() ERROR_SEVERITY,ERROR_STATE() ERROR_STATE
             END CATCH
            
         END
@@ -30,6 +37,8 @@ BEGIN
         SELECT 'Wrong Input Parameters'
 
 END
+
+
 
 
 
@@ -43,7 +52,7 @@ SELECT @TotalEmployee as TotalEmployee
 
 
 go
-DECLARE @AVG_Amount MONEY, @TotalEmployee int;
+DECLARE @AVG_Amount int, @TotalEmployee int;
 EXEC average_employee_amount @EmployeeFrom = 123, @EmployeeTo = 222, @AVG_Amount = @AVG_Amount OUTPUT , @TotalEmployee = @TotalEmployee output;
 SELECT @AVG_Amount AVG
 SELECT @TotalEmployee as TotalEmployee
