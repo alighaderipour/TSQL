15e2971a1a0b8acbf6fb86b428539600508bb5e7
Shahaz <ali.ghaderipour@gmail.com>
2025-02-05 12:32:22 +0330

    work
    1403-11-17 12:32

--- Northwind/practice.sql
+++ Northwind/practice.sql
@@ -1,14 +1,11 @@
-/*
-28. High freight charges - last year
-We're continuing to work on high freight charges. We now want to get
-the three ship countries with the highest average freight charges. But
-instead of filtering for a particular year, we want to use the last 12
-months of order data, using as the end date the last OrderDate in Orders.
-*/
-WITH cte AS (
-SELECT max(orderdate) maxed_order from orders 
-)
-SELECT top 3  o.ShipCountry, avg(freight) totalavg from Orders o
-WHERE o.OrderDate > DATEADD(year, -1, (SELECT maxed_order from cte))
-group BY o.ShipCountry
-order BY totalavg desc
+SELECT * FROM studentsSectionWise
+SELECT studentID FROM studentsSectionWise
+-----------------------------
+
+select db_name() CurrentDatabase
+select getdate() CurrentDate
+select user_name() CurrentUser
+select @@servername CurrentServer
+select @@version CurrentVersion 
+
+
