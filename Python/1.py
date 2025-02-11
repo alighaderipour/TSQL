644913051ad189c27a6934a1fb890e001bc08451
Shahaz <ali.ghaderipour@gmail.com>
2025-02-11 12:53:13 +0330

    work
    1403-11-23 1300

--- Python/1.py
+++
@@ -1,9 +0,0 @@
-# https://realpython.com/python3-object-oriented-programming/?spm=5aebb161.1b0c161.0.0.7dfd5171uyg1jG
-class Employee:
-    def __init__(self, name, age):
-        self.name =  name
-        self.age = age
-
-e1 = Employee("Ali" , 31)
-print(e1.name)
-print(e1.age)
