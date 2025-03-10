b249f1c858c48081f93a3240144dfe5fb06706cb
Shahaz <ali.ghaderipour@gmail.com>
2025-03-10 08:28:45 +0330

    work 1403-12-20
    some changes in question 20

--- Northwind/20.sql
+++ Northwind/20.sql
@@ -5,6 +5,18 @@ category. Sort the results by the total number of products, in descending
 order.
 */
 
+SELECT c.CategoryName, p.ProductID  FROM Products p INNER JOIN Categories c ON p.CategoryID = c.CategoryID
+SELECT DISTINCT
+    c.CategoryName, COUNT(p.ProductID) OVER (PARTITION BY c.CategoryName) AS TotalProducts
+FROM Products p
+INNER JOIN Categories c ON p.CategoryID = c.CategoryID
+ORDER BY TotalProducts DESC;
+
+
+-------------------------------------
+
+
+
 SELECT c.CategoryID ,count(ProductID) as total FROM Categories c inner join Products p ON c.CategoryID = p.CategoryID GROUP BY c.CategoryID
 order by total DESC
 
@@ -17,3 +29,15 @@ select c.categoryname, c1.total from Categories c
 inner join cte as c1
 on c.CategoryID = c1.CategoryID
 order by total desc
+
+----
+Select
+CategoryName
+,TotalProducts = count(*)
+From Products
+Join Categories
+on Products.CategoryID = Categories.CategoryID
+Group by
+CategoryName
+Order by
+count(*) desc
